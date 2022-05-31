76017cd54b99bb9ecc69528bc427b8848db2916c
Dane <dane@dbridgman.co.uk>
2022-05-31 19:59:57 +0100

    Player controls, game assets and collision detection

---
+++ Assets/Collision.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collision : MonoBehaviour
+{
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        Debug.Log("Owwww!");
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Ehhhh?");
+    }
+}
--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
+    [SerializeField]
+    float steerSpeed = 300f;
+
+    [SerializeField]
+    float moveSpeed = 20f;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +20,10 @@ public class Driver : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(0, 0, 0.5f);
+        float steerAmount = Input.GetAxis("Horizontal") * steerSpeed * Time.deltaTime;
+        float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+
+        transform.Rotate(0, 0, -steerAmount);
+        transform.Translate(0, moveAmount, 0);
     }
 }
